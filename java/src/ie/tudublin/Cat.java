3fd66b6652deb665bd2b11c1664c89374f864f61
ontingleung <ontingleung@tutamail.com>
2023-01-25 23:14:09 +0000

    killing the cat

---
+++ java/src/ie/tudublin/Cat.java
@@ -0,0 +1,23 @@
+package ie.tudublin;
+
+public class Cat {
+    
+    int numLives;
+    String name;
+
+    public Cat(String name) {
+        this.name = name;
+        this.numLives = 9;
+    }
+
+	public void kill() {
+		this.numLives = this.numLives - 1;
+        if (this.numLives > 0) {
+            System.out.println("Ouch!");
+        }
+        else {
+            System.out.println("Dead");
+        }
+	}
+
+}
