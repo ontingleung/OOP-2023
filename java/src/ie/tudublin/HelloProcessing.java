2485cd7a5ab2698ce883a907116833640adf591e
ontingleung <ontingleung@tutamail.com>
2023-01-30 12:49:10 +0000

    processing class

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,15 +11,26 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		
+		background(255,0,0);
 	}
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
-	
 	public void draw()
 	{	
-		
+		fill(255,255,0);
+		circle(250, 250, 400); // (cx, cy, d)
+		fill(0,255,255);
+		triangle(250, 10, 50, 400, 450, 400);
+		//rect(10, 10, 10, 100); // tlx, tly, w, h
+		fill(255);
+		ellipse(mouseX, mouseY, 200, 100);
+		fill(0);
+		circle(mouseX, mouseY, 100); // (cx, cy, d)
+
+		if (mousePressed) {
+			fill(0);
+			ellipse(250, 230, 200, 100);
+			fill(255,150,0);
+			line(200, 260, 55, 55);
+		}
 	}
 }
