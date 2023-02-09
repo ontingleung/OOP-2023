8c706fca1569678ec5efebb64317734e695d639b
ontingleung <ontingleung@tutamail.com>
2023-02-09 10:00:03 +0000

    circles

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -5,33 +5,46 @@ import processing.core.PApplet;
 public class Loops extends PApplet {
 
 	int mode = 0;
+
 	public void settings() {
 		size(1000, 1000);
 	}
 
+	float width = 1000;
+	float height = 1000;
+
 	public void setup() {
-		background(255, 0, 0);
+		colorMode(HSB);
 	}
 
-
-
 	public void keyPressed() {
-		
+
 		mode = key - '0';
 		println(mode);
 	}
 
+	float offset = 0;
+
 	public void draw() {
 		background(255);
-		fill(255);
-		
-		for(int i = 10; i < 10; i++) {
-			for(int j = 10; j < 10; j++) {
-				colorMode(HSB);
-				circle((10*i), (10*j), 100);
+		noStroke();
+
+
+		int circles = (int) (mouseX / 100.0f);
+		float d = width / (float) circles;
+		for (int j = 0; j < circles; j++) {
+			for (int i = 0; i < circles; i++) {
+				noStroke();
+				float c = map((i + j + offset), 0, circles * 2, 0, 255) % 256;
+				fill(c, 255, 255);
+				float x = map(i, 0, circles - 1, d / 2.0f, width - (d / 2.0f));
+				float y = map(j, 0, circles - 1, d / 2.0f, width - (d / 2.0f));
+				fill(c + offset, 255, 255);
+				circle(x, y, d);
 			}
 		}
-		
+
+		offset *= (mouseY / 50.0f);
 
 	}
 }
