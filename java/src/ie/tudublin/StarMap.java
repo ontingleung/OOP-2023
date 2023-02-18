cd818d63bf3ed37f9bfb5f5e3cd81c0ee226e95a
ontingleung <ontingleung@tutamail.com>
2023-02-18 14:25:44 +0000

    text

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -16,13 +16,37 @@ public class StarMap extends PApplet
 	public void setup() {
 		colorMode(RGB);
 		background(0);
-		
+		stroke(221,160,221);
 		smooth();
 
 	}
 
-	public void draw()
-	{	
+	public void loadData() {
 
 	}
+
+	public void drawGrid() {
+		float border = width * 0.1f;
+		float lines = 10.0f;
+		float gap = (width - (border*2.0f)) / lines; 
+
+
+		for(int i = -5; i <= 5; i++) {
+			float x = border + (gap * (i+5));
+			float y = border;
+			line(x, y, x, height - border);
+			line(y, x, width - border, x);
+
+			textAlign(CENTER, CENTER);
+			text(i, x, gap * 0.5f);
+			text(i, gap * 0.5f, x);
+		}
+	}
+
+	public void draw() {	
+		strokeWeight(2);
+
+		drawGrid();
+	}
+
 }
