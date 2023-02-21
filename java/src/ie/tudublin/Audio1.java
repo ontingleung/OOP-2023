3899c051f7eb51ae0ed1b0affbfe77fc759048fd
ontingleung <ontingleung@tutamail.com>
2023-02-21 14:49:57 +0000

    wow

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -16,12 +16,21 @@ public class Audio1 extends PApplet
 	
 	public void settings()
 	{
-		size(500, 500);
+		size(1024, 500);
 	}
+
+	int frameSize = 1024;
+
 
 	public void setup() {
 		colorMode(HSB);
 		background(0);
+
+		minim = new Minim(this);
+
+		ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16); // (int type, int bufferSize, float sampleRate, int bitDepth)
+ 
+		ab = ai.mix;
 
 	
 		smooth();
@@ -30,6 +39,33 @@ public class Audio1 extends PApplet
 	
 	public void draw()
 	{	
-	
+		background(0);
+		stroke(255);
+
+		float half = height / 2;
+		float cgap = 255 / (float)ab.size();
+
+		float total = 0;
+		for (int i = 0; i < ab.size(); i++) {
+
+			total += abs(ab.get(i));
+			stroke(cgap * i, 255, 255);
+			line(i, half, i, half + ab.get(i) * half);
+			//circle(height / 2 ,width / 2,abs( half + ab.get(i)));
+		}
+
+		float average = total / (float)ab.size();
+
+		float r = average * 200;
+
+		circle(100, 200, r);
+
+		lerpedR = lerp(lerpedR, r, 0.1f);
 	}
+
+	float lerp1(float a, float b, float t) {
+		return a + (b - a) * t;
+	}
+
+	float lerpedR = 0;
 }
