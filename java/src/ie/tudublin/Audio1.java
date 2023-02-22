17e9ba851a104298ea15c9d126b558ec7b1cf10e
ontingleung <ontingleung@tutamail.com>
2023-02-22 23:16:29 +0000

    part2

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -92,7 +92,13 @@ public class Audio1 extends PApplet
                 }
                 break;
         case 1:
-            background(0);            
+            background(0);
+            for (int i = 0; i < ab.size(); i++) {
+                float c = map(i, 0, ab.size(), 0, 255);
+                stroke(c, 255, 255);
+                float f = ab.get(i) * halfH;
+                line(i, halfH + f, i, halfH - f);
+            }            
             break;
 
         }
