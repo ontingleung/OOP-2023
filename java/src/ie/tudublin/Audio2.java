7d930db327c800e9a0ac3df2e194fa02191d2d4d
ontingleung <ontingleung@tutamail.com>
2023-02-27 11:48:13 +0000

    hello

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -36,4 +36,7 @@ public class Audio2 extends PApplet
             line(i, half, i, half + ab.get(i) * half);
         }
     }        
+    static public final float map(float value, float istart, float istop, float ostart, float ostop) {
+        return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+  }
 }
