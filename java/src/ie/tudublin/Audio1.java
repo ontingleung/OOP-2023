f3a31f1538d9fb1d1e8833a333b6223c709c68a6
ontingleung <ontingleung@tutamail.com>
2023-02-27 11:32:43 +0000

    week6

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -13,25 +13,6 @@ public class Audio1 extends PApplet
     AudioInput ai;
     AudioBuffer ab;
 
-    int mode = 0;
-
-    float y = 0;
-    float smoothedY = 0;
-    float smoothedAmplitude = 0;
-
-    public void keyPressed() {
-		if (key >= '0' && key <= '9') {
-			mode = key - '0';
-		}
-		if (keyCode == ' ') {
-            if (ap.isPlaying()) {
-                ap.pause();
-            } else {
-                ap.rewind();
-                ap.play();
-            }
-        }
-	}
 
     public void settings()
     {
@@ -52,89 +33,12 @@ public class Audio1 extends PApplet
         ab = ap.mix;
         colorMode(HSB);
 
-        y = height / 2;
-        smoothedY = y;        
+        y = height / 2;      
     }
 
-    float off = 0;
-
-    float lerpedBuffer[] = new float[1024];
 
     public void draw()
     {
-        //background(0);
-        float halfH = height / 2;
-        float average = 0;
-        float sum = 0;
-        off += 1;
-
-
-        // Calculate sum and average of the samples
-        // Also lerp each element of buffer;
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
-            sum += abs(ab.get(i));
-            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
-        }
-        average= sum / (float) ab.size();
-
-        smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
         
-        float cx = width / 2;
-        float cy = height / 2;
-
-        switch (mode) {
-			case 0:
-                background(0);
-                for(int i = 0 ; i < ab.size() ; i ++)
-                {
-                    //float c = map(ab.get(i), -1, 1, 0, 255);
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    float f = lerpedBuffer[i] * halfH * 4.0f;
-                    line(i, halfH + f, i, halfH - f);                    
-                }
-                break;
-        case 1:
-            background(0);
-            for(int i = 0 ; i < ab.size() ; i ++)
-            {
-                //float c = map(ab.get(i), -1, 1, 0, 255);
-                float c = map(i, 0, ab.size(), 0, 255);
-                stroke(c, 255, 255);
-                float f = lerpedBuffer[i] * halfH * 4.0f;
-                line(i, halfH + f, halfH - f, i);                    
-            }
-            break;      
-        case 2:
-            background(0);
-            for(int i = 0 ; i < ab.size() ; i ++)
-            {
-                float c = map(i, 0, ab.size(), mouseX /2, mouseY/ 2);
-                stroke(c, 255, 255);
-                float f = lerpedBuffer[i] * halfH * 4.0f;
-                line(0, i, f, i);              
-                line(width, i, width - f, i);              
-                line(i, 0, i, f);          
-                line(i, height, i, height - f);              
-            }
-            break;          
-        }
-
-
-        
-        // Other examples we made in the class
-        /*
-        stroke(255);
-        fill(100, 255, 255);        
-        
-        circle(width / 2, halfH, lerpedA * 100);
-
-        circle(100, y, 50);
-        y += random(-10, 10);
-        smoothedY = lerp(smoothedY, y, 0.1f);        
-        circle(200, smoothedY, 50);
-        */
-
     }        
 }
